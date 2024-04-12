c599133fe1180d364786cc7a015df0b5cbedceb4
skranz-auto <xsx50@uni-ulm.de>
2024-04-12 23:36:16 +0000

    Update from server

--- R/map_options.R
+++ R/map_options.R
@@ -4,14 +4,18 @@ repbox_map_opts = function(
   reg_min_match_score = 0.358,
   approx_map_max_deci_dist = 2, # measured in decis
   table_ref_differs_shrink_factor = 0.9,
-  reg_vs_noreg_map_factor = 0.8
+  reg_vs_noreg_map_factor = 0.8,
+  reg_ignore_missing_data = TRUE,
+  noreg_ignore_missing_data = TRUE
 ) {
  list(
    reg_min_match_score_by_art_ncoef=reg_min_match_score_by_art_ncoef,
    reg_min_match_score = reg_min_match_score,
    approx_map_max_deci_dist = approx_map_max_deci_dist,
    table_ref_differs_shrink_factor = table_ref_differs_shrink_factor,
-   reg_vs_noreg_map_factor = reg_vs_noreg_map_factor
+   reg_vs_noreg_map_factor = reg_vs_noreg_map_factor,
+   reg_ignore_missing_data = reg_ignore_missing_data,
+   noreg_ignore_missing_data = noreg_ignore_missing_data
   )
 
 }
